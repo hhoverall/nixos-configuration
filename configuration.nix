55815d76429c5eed1786e623405b0ee10ba86689
hhoverall <hove0001@student.monash.edu>
2025-09-28 12:17:37 +1000

    Initial commit

---
+++ configuration.nix
@@ -0,0 +1,188 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, lib, secrets, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ./nova/nixfiles/nixos
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Australia/Melbourne";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_AU.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_AU.UTF-8";
+    LC_IDENTIFICATION = "en_AU.UTF-8";
+    LC_MEASUREMENT = "en_AU.UTF-8";
+    LC_MONETARY = "en_AU.UTF-8";
+    LC_NAME = "en_AU.UTF-8";
+    LC_NUMERIC = "en_AU.UTF-8";
+    LC_PAPER = "en_AU.UTF-8";
+    LC_TELEPHONE = "en_AU.UTF-8";
+    LC_TIME = "en_AU.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "au";
+    variant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Uninstall chrome
+  programs.chromium.enable = false;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #  wget
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.05"; # Did you read the comment?
+
+  nova.profile = "shared";
+  nova.substituters.nova.password = secrets.hydra-password;
+  home-manager.sharedModules = [{
+    home.stateVersion = "23.05";
+  }];
+  home-manager.backupFileExtension = "backup";
+
+   home-manager.users.nova = {
+    home.packages = with pkgs; [
+      slack
+      discord
+      spotify
+      blackbox-terminal
+      # discord-screenaudio
+
+      tmux
+      nix-output-monitor
+    ];
+
+    # git profile
+    programs.git = lib.mkForce {
+      enable = true;
+      userName = "hhoverall";
+      userEmail = "hove0001@student.monash.edu";
+    };
+
+    # Add to taskbar
+    dconf.settings."org/gnome/shell".favorite-apps = [
+      "firefox.desktop"
+      "slack.desktop"
+      "discord.desktop"
+      "spotify.desktop"
+    ];
+
+    dconf.settings = {
+      # Adds HiDPI scaling support
+      "org/gnome/mutter".experimental-features = [
+        "scale-monitor-framebuffer"
+      ];
+
+      # Fix annoying things
+      "org/gnome/desktop/interface" = {
+        # This garbage is so annoying, prevents programs from capturing control being pressed
+        locate-pointer = lib.mkForce false;
+      };
+      "org/gnome/desktop/sound" = {
+        # I enjoy not ruining my audio :)
+        allow-volume-above-100-percent = lib.mkForce false;
+      };
+      "org/gnome/desktop/session" = {
+        idle-delay = lib.mkForce (120 * 60); # 2 hours on my desktop
+      };
+      "org/gnome/settings-daemon/plugins/power" = {
+        sleep-inactive-ac-timeout = 2700; # 3x the default
+      };
+      "org/gnome/desktop/screensaver" = {
+        lock-delay = 3600;
+      };
+    };
+
+
+    };
+  
+}
