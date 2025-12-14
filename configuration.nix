121d6c25abaf433d2513d09421d63b2f0476f162
hhoverall <hove0001@student.monash.edu>
2025-12-14 18:03:49 +1100

    Add NVIDIA graphics card settings to config.

--- configuration.nix
+++ configuration.nix
@@ -118,6 +118,9 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.05"; # Did you read the comment?
 
+  # fix time issue when dual booting
+  time.hardwareClockInLocalTime = true;
+
   nova.profile = "shared";
   nova.substituters.nova.password = import ./hydra-secret.nix;
   home-manager.sharedModules = [{
@@ -183,5 +186,47 @@
 
     # Disables GNOME default utilities
     # services.gnome.core-utilities.enable = false;
+
+
+    # NVIDIA graphics card stuff
+    # https://nixos.wiki/wiki/Nvidia
+    # Enable OpenGL
+  hardware.graphics = {
+    enable = true;
+  };
+
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+
+    # Modesetting is required.
+    modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    # Enable this if you have graphical corruption issues or application crashes after waking
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+    # of just the bare essentials.
+    powerManagement.enable = false;
+
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of 
+    # supported GPUs is at: 
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+    # Only available from driver 515.43.04+
+    open = true;
+
+    # Enable the Nvidia settings menu,
+	# accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
 
 }
